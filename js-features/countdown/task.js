fb623e98680fb8ec34b1125a795f95c715ba32ef
rupersuper <rupersuper@mail.ru>
2024-03-18 22:45:56 +0600

    Доп задание Таймер обратного отсчёта выполнено

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,15 +1,23 @@
 const timerElement = document.getElementById("timer");
 let timeLeft = parseInt(timerElement.textContent);
 
-function updatetimer() {
-  timeLeft--;
+function updateTimer() {
+  let hours = Math.floor(timeLeft / 3600);
+  let minutes = Math.floor((timeLeft % 3600) / 60);
+  let seconds = timeLeft % 60;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  hours = hours < 10 ? "0" + hours : hours;
+
   if (timeLeft < 0) {
     clearInterval(timerInterval);
-    timeLeft = 0;
-    alert("Вы победили в конкурсе!");
+    // alert("Вы победили в конкурсе!");
+    window.location.href =
+      "https://github.com/rupersuper/bhj-homeworks/archive/refs/heads/master.zip";
   }
 
-  timerElement.textContent = timeLeft;
+  timerElement.textContent = `${hours}:${minutes}:${seconds}`;
+  timeLeft--;
 }
 
-const timerInterval = setInterval(updatetimer, 1000);
+const timerInterval = setInterval(updateTimer, 1000);
