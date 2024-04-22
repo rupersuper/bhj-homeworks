aacb924b01e9f6d5a0551227cc39dc05202727c4
rupersuper <rupersuper@mail.ru>
2024-04-22 10:52:05 +0500

    async-requests refactor

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -5,23 +5,28 @@ const poolAnswers = document.querySelector(".poll__answers");
 xhr.open("GET", "https://students.netoservices.ru/nestjs-backend/poll");
 
 xhr.onload = function () {
-  const pool = JSON.parse(xhr.response);
-  const {
-    data: { answers, title },
-  } = pool;
+  if (xhr.status === 200) {
+    const pool = JSON.parse(xhr.response);
+    const {
+      data: { answers, title },
+    } = pool;
 
-  answers.forEach((answer) => {
-    poolTitle.innerHTML = title;
-    const poolAnswer = `<button class="poll__answer">${answer}</button>`;
-    poolAnswers.innerHTML += poolAnswer;
-  });
-
-  document.querySelectorAll(".poll__answer").forEach((item) => {
-    item.addEventListener("click", (event) => {
-      event.preventDefault();
-      alert("Спасибо, ваш голос засчитан!");
+    answers.forEach((answer) => {
+      poolTitle.innerHTML = title;
+      const poolAnswer = `<button class="poll__answer">${answer}</button>`;
+      poolAnswers.innerHTML += poolAnswer;
     });
-  });
+
+    document.querySelectorAll(".poll__answer").forEach((item) => {
+      item.addEventListener("click", (event) => {
+        event.preventDefault();
+        alert("Спасибо, ваш голос засчитан!");
+        location.reload();
+      });
+    });
+  } else {
+    console.log("Ошибка " + xhr.status);
+  }
 };
 
 xhr.send();
